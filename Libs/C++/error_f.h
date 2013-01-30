d83e2f6e2e535768bdd33d80d7ca9206fb318846
Gabriel Ferrin <gaf11f@my.fsu.edu>
2013-01-30 14:43:48 -0500

    finshed error_f

--- Libs/C++/error_f.h
+++ Libs/C++/error_f.h
@@ -2,12 +2,13 @@
 #define ERROR_F_H_
 
 #include <cmath>
+#include "gamma_p.h"
 
 double error_f( double X );
 
 double error_f( double X )
 {
-	double error = sin(X)
+	double error = sin(X) * gamma_p( 0.5, ( X * X )); // Incomplete gamma function.
 }
 
 #endif /* ERROR_F_H_ */
